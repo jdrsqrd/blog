dca9a60a4f621288a3c7bdb290c8b6cd50cdf702
Johnathan <jdrsqrd@gmail.com>
2013-08-26 12:12:46 -0400

    initial commit

---
+++ vendor/bundle/gems/atomic-1.1.13/ext/siteconf20130826-13413-yc5fpm.rb
@@ -0,0 +1,6 @@
+require 'rbconfig'
+dest_path = "./.gem.20130826-13413-4fg8tg"
+RbConfig::MAKEFILE_CONFIG['sitearchdir'] = dest_path
+RbConfig::CONFIG['sitearchdir'] = dest_path
+RbConfig::MAKEFILE_CONFIG['sitelibdir'] = dest_path
+RbConfig::CONFIG['sitelibdir'] = dest_path
